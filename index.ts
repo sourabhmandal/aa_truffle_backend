8f61afeac10fb95f756e834bf4ad94d2e668721c
Sourabh Mandal <39632788+sourabhmandal@users.noreply.github.com>
2021-10-06 18:43:55 +0530

    choose port by cloud

--- index.ts
+++ index.ts
@@ -13,6 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/aa", aaRouter);
 
-app.listen(port, () => {
+app.listen(process.env["PORT"] || port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
