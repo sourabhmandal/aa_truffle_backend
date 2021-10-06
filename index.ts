0bc4fb90ce0291e9ac92ea7fafad05a1f027adea
Sourabh Mandal <39632788+sourabhmandal@users.noreply.github.com>
2021-10-06 18:46:36 +0530

    add root route

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 "use strict";
 var cors = require("cors");
+import { Request, Response } from "express";
 import "reflect-metadata";
 
 import { aaRouter } from "./routes/accountAggregator";
@@ -11,7 +12,12 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/aa", aaRouter);
+app.get("/", (req: Request, res: Response) => {
+  res.send("TRUFFLE SERVICE IS UP");
+});
+
 
 app.listen(process.env["PORT"] || port, () => {
   console.log(`App listening at http://localhost:${port}`);
